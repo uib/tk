63bb493b2b3986e4b270b4c7a638e06380fac4ed
Gisle Aas <gisle.aas@it.uib.no>
2013-12-16 15:28:32 +0100

    Ensure writeable database, also for Apache

--- scripts/install-sqlite.sh
+++ scripts/install-sqlite.sh
@@ -4,15 +4,21 @@ set -x
 
 cd ../drupal || exit 1
 
-SETTINGS=sites/default/settings.php
+SITE=sites/default
+SETTINGS=$SITE/settings.php
+FILES=$SITE/files
+DB=$FILES/.ht.sqlite
+
 chmod u+w $SETTINGS
-cp sites/default/default.settings.php $SETTINGS
+cp $SITE/default.settings.php $SETTINGS
 chmod u+w $SETTINGS
 
-mkdir sites/default/files
-chmod 777 sites/default/files
+mkdir $FILES
+chmod 777 $FILES
 
+rm -f $DB
 drush site-install standard --account-pass=admin --site-name='Tjenestekatalogen' --db-url=sqlite:sites/default/files/.ht.sqlite -y
 chmod 444 $SETTINGS
+chmod 777 $DB
 
 drush en tk diff -y
