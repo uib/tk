8822b8dcfcbe1b189a52e5c368bf10aa28f2e984
Gisle Aas <gisle.aas@it.uib.no>
2014-01-22 10:15:06 +0100

    Ensure clean database before we restore

--- scripts/pg-restore.sh
+++ scripts/pg-restore.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+(cd drupal && drush sql-drop)
 perl -pe 's/tk_(user|admin)/user1/g' tk.database.sql  | (cd drupal && drush sqlc)
 (cd drupal && drush vset ldap_servers_require_ssl_for_credentails 0)
