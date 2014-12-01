d28f7ebc55f02d07d63bb83f9c3b591e9e5a9f48
Gisle Aas <gisle.aas@it.uib.no>
2014-12-01 19:49:38 +0100

    Publish all services
    Tried to publish them all with a simple 'update node set status=1'
    but that appares to not be the whole story.  The pages still unpublised
    in the rendered view.
    
    Saving the nodes properly fixes this problem.

---
+++ scripts/fixup-unpub.php
@@ -0,0 +1,13 @@
+<?php
+
+$result = (new EntityFieldQuery())
+  ->entityCondition('entity_type', 'node')
+  ->entityCondition('bundle', 'service')
+  ->execute();
+$nids = array_keys($result['node']);
+$nodes = node_load_multiple($nids);
+
+foreach ($nodes as $node) {
+    $node->status = 1;
+    node_save($node);
+}
