b712bbd9174a523c002862e4729b960a6fea4b93
Gisle Aas <gisle.aas@it.uib.no>
2014-12-19 15:16:40 +0100

    Only touch those in need

--- scripts/fixup-unpub.php
+++ scripts/fixup-unpub.php
@@ -5,11 +5,16 @@ $q = new EntityFieldQuery();
 $result = $q
   ->entityCondition('entity_type', 'node')
   ->entityCondition('bundle', 'service')
+  ->propertyCondition('status', 0)
   ->execute();
-$nids = array_keys($result['node']);
-$nodes = node_load_multiple($nids);
 
-foreach ($nodes as $node) {
-    $node->status = 1;
-    node_save($node);
+if (isset($result['node'])) {
+  $nids = array_keys($result['node']);
+  $nodes = node_load_multiple($nids);
+
+  foreach ($nodes as $node) {
+      print "/node/$node->nid $node->title\n";
+      $node->status = 1;
+      node_save($node);
+  }
 }
